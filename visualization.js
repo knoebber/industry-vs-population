8601beaa2394fac90adba02b66fbd703b8a3cd14
knoebber <knoebber@gmail.com>
2017-05-05 14:55:34 -0500

    update year list depending on data, create functions

--- visualization.js
+++ visualization.js
@@ -19,34 +19,52 @@ function drawCircle(coordinates) {
      .attr('stroke','red')
      .attr('stroke-width','2');
 }
+
+function updateYearSelect(data) {
+  d3.select('#year-dropdown')
+    .selectAll('option')
+    .remove();
+
+  d3.select('#year-dropdown')
+    .selectAll('option')
+    .data(data)
+    .enter()
+    .append('option')
+    .attr('value', d => d.Year)
+    .text(d=>d.Year);
+}   
 // Generate an SVG element on the page
-var svg = d3.select("body").append("svg")
-  .attr("width", svg_width - 50)
-  .attr("height", svg_height)
+var svg = d3.select('body').append('svg')
+  .attr('width', svg_width - 50)
+  .attr('height', svg_height)
   .attr('style', 'border:solid 3px #212121; padding: 30px 0; margin: 15px 0');
 
 
 d3.queue()
-  .defer(d3.json, "us.json")
-  .defer(d3.csv, "data/detroit.csv")
-  .defer(d3.csv, "data/goldrush.csv")
-  .defer(d3.csv, "data/northdakotapop.csv")
+  .defer(d3.json, 'us.json')
+  .defer(d3.csv, 'data/detroit.csv')
+  .defer(d3.csv, 'data/goldrush.csv')
+  .defer(d3.csv, 'data/northdakotapop.csv')
   .await(function(error, usa, detroit,SF,ndakota) {
 
         if (error) console.error('error loading data: ' + error);
         else
         {
 
-          d3.select("#dropdown").on('change',function() { 
+          d3.select('#industry-dropdown').on('change',function() { 
+            var yearSelect = d3.select('#year-dropdown');
             switch(this.value){
               case 'automotive' :
-                drawCircle(detroitXY); 
-                break;
+               drawCircle(detroitXY); 
+               updateYearSelect(detroit);
+               break;
               case 'mining' :
                 drawCircle(sanFranXY);
+                updateYearSelect(SF);
                 break;
               case 'oil' :
                 drawCircle(nDakotaXY); 
+                updateYearSelect(ndakota);
                 break;
              }
           });
@@ -64,7 +82,7 @@ d3.queue()
              .append('path')
              .attr('fill','white')
              .attr('d', path)
-             .attr('stroke', "#212121")
+             .attr('stroke', '#212121')
              .attr('stroke-width', 2);
         }
 });

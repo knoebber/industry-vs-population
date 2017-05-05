bac506643beb464ecd693ae108f7a67f60918d1a
Nicolas Knoebber <knoebber@gmail.com>
2017-05-05 12:35:14 -0500

    add code to draw circle on selected city

--- visualization.js
+++ visualization.js
@@ -4,6 +4,14 @@ var svg_height = window.innerHeight;
 
 var projection = d3.geoAlbersUsa();
 var path = d3.geoPath().projection(projection);
+//coordinates of cities we will be looking at
+var sanFranXY = projection([37.46,122.55]); //these seem to be wrong
+var detroitXY = projection([-83.04,42.33]);
+var nDakotaXY = projection([47.57,102.84]); //and this oen
+console.log(sanFranXY);
+console.log(detroitXY);
+console.log(nDakotaXY);
+
 // Generate an SVG element on the page
 var svg = d3.select("body").append("svg")
   .attr("width", svg_width - 50)
@@ -19,10 +27,22 @@ d3.queue()
   .await(function(error, usa, detroit,SF,ndakota) {
 
         if (error) console.error('error loading data: ' + error);
-        else 
+        else
         {
 
-          d3.select("#dropdown").on('change',function() { console.log("changed");});
+          d3.select("#dropdown").on('change',function() { 
+            switch(this.value){
+              case 'automotive' :
+                svg.append('circle')
+                   .attr('r',4)
+                   .attr('cx',detroitXY[0])
+                   .attr('cy',detroitXY[1])
+                   .attr('fill','none')
+                   .attr('stroke','red')
+                   .attr('stroke-width','2');
+                break;
+                }
+          });
 
           svg.append('path')
              .datum(topojson.feature(usa, usa.objects.land))
